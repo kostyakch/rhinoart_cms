662fb00bbce15aec8e13ae43f3b29d254fb332f0
KCh <kch@list.ru>
2014-08-17 14:06:02 +0400

    not remember

--- app/models/rhinoart/gallery.rb
+++ app/models/rhinoart/gallery.rb
@@ -17,8 +17,8 @@ module Rhinoart
 	class Gallery < ActiveRecord::Base
 		#3attr_accessible :active, :descr, :name, :url
 
-		has_many :gallery_image, :order => 'position', :autosave => true, :dependent => :destroy
-		accepts_nested_attributes_for :gallery_image, :allow_destroy => true
+		has_many :gallery_images, :order => 'position', :autosave => true, :dependent => :destroy
+		accepts_nested_attributes_for :gallery_images, :allow_destroy => true
 
 		# Validations
 		validates :name, :length => { :in => 2..150 }, uniqueness: true
