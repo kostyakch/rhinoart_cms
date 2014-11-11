395dcb149c5dd00dd6edcfd7ba24ce5306a2e31d
KCh <kch@list.ru>
2014-11-11 21:57:11 +0300

    Add sefault settings value

--- db/migrate/20141111175442_change_settings_value.rb
+++ db/migrate/20141111175442_change_settings_value.rb
@@ -1,8 +1,7 @@
 class ChangeSettingsValue < ActiveRecord::Migration
 	def change
 		remove_index :rhinoart_settings, [:name, :value]
-		change_column :rhinoart_settings, :value, :text, :null => true
-
+		change_column :rhinoart_settings, :value, :text, :null => true, :default => nil
 		add_index :rhinoart_settings, :name, :unique => true 
 	end
 end
