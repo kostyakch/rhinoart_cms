e622532140d872c83aa44da78188af200117710e
Shredder <shredder-rull@yandex.ru>
2015-03-20 00:44:42 +0600

    Some fixes. Added registration assets

--- lib/rhinoart.rb
+++ lib/rhinoart.rb
@@ -1,11 +1,22 @@
 require 'rhinoart/engine'
+require 'rhinoart/asset_registration'
 
 module Rhinoart
   extend ActiveSupport::Autoload
+  extend AssetRegistration
 
   eager_autoload do
     autoload :Menu
     autoload :ViewHelpers
+  end
+
+  def self.setting(name, value = nil)
+    mattr_accessor name.to_sym
+    self.send("#{name}=", value)
+  end
+
+  def self.setup
+    yield self
   end
 
 end
--- lib/rhinoart/railtie.rb
+++ lib/rhinoart/railtie.rb
@@ -4,6 +4,15 @@ module Rhinoart
     VIEW_HELPERS_PATH = 'lib/rhinoart/view_helpers/'
 
     initializer "rhinoart_railtie.add_precompile_assets" do |app|
+
+      Rhinoart.stylesheets.each do |path, _|
+        app.config.assets.precompile << path
+      end
+
+      Rhinoart.javascripts.each do |path|
+        app.config.assets.precompile << path
+      end
+
       app.config.assets.precompile += %w[
         jquery.tablesorter.js
         redactor/redactor.css
