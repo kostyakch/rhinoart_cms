2748bb6b93174db815c6ff173ec7bc4f18d65a4a
Shredder <shredder-rull@yandex.ru>
2015-04-04 01:16:41 +0600

    Added menu methods

--- lib/rhinoart/menu/base.rb
+++ lib/rhinoart/menu/base.rb
@@ -10,6 +10,17 @@ module Rhinoart
         self.items << item_class.new(params)
       end
 
+      def prepend_item(params = {})
+        self.items ||= []
+        self.items.unshift item_class.new(params)
+      end
+
+      def insert(index = 0, params = {})
+        self.items ||= []
+        index = self.items[index].present? ? index : self.items.size
+        self.items.insert index, item_class.new(params)
+      end
+
       def self.add_item(params)
         self.instance.add_item(params)
       end
