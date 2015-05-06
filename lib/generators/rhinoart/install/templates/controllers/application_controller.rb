d72d86648c818b1ec5856f511911f7c5f5991811
KCh <kch@list.ru>
2015-05-06 15:58:13 +0300

    Small fixes

--- lib/generators/rhinoart/install/templates/controllers/application_controller.rb
+++ lib/generators/rhinoart/install/templates/controllers/application_controller.rb
@@ -3,10 +3,19 @@ class ApplicationController < ActionController::Base
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
 	before_filter :check_uri
+	before_action :set_locale
 
 	def check_uri
 		if Rails.configuration.redirect_to_www
 			redirect_to request.protocol + "www." + request.host_with_port + request.fullpath if !/^www/.match(request.host)
 		end
 	end
+
+	def set_locale
+		if current_user.present? && current_user.locales.present? && current_user.locales.count == 1
+			I18n.locale = params[:locale] || current_user.locales.first || I18n.default_locale
+		else
+			I18n.locale = params[:locale] || I18n.default_locale
+		end
+	end	
 end
