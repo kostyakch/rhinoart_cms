2455efe2dece406910046cb3fad32d9762350674
KCh <kch@list.ru>
2015-06-04 16:20:18 +0300

    Make omniauth_callbacks redirect after sign_in

--- app/controllers/rhinoart/omniauth_callbacks_controller.rb
+++ app/controllers/rhinoart/omniauth_callbacks_controller.rb
@@ -7,7 +7,10 @@ module Rhinoart
 
       if @user.persisted?
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-        sign_in_and_redirect @user, :event => :authentication
+        # sign_in_and_redirect @user, :event => :authentication
+        sign_in @user, :event => :authentication
+        redirect_to session[:redirect_to] || main_app.root_path
+
       else
         session["devise.google_data"] = request.env["omniauth.auth"]
         redirect_to new_user_registration_url
