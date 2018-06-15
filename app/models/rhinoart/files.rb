d490a237d9c405e4a08567adb1c5948a2e57e443
KCh <kch@list.ru>
2018-06-15 09:54:25 +0300

    Minifixes

--- app/models/rhinoart/files.rb
+++ app/models/rhinoart/files.rb
@@ -12,24 +12,24 @@
 #
 
 module Rhinoart
-	class Files < ActiveRecord::Base
-		self.inheritance_column = "non_sti"
+  class Files < ActiveRecord::Base
+    self.inheritance_column = 'non_sti'
 
-		belongs_to :attachable, polymorphic: true
+    belongs_to :attachable, polymorphic: true
 
-		validates :file, presence: true
+    validates :file, presence: true
 
-		FILE_TYPE_PAGE_FILED = "page_filed"
-		FILE_TYPES = local_constants.select { |const| const.to_s.starts_with? "VFILE_TYPE_" }		
+    FILE_TYPE_PAGE_FILED = 'page_filed'.freeze
+    FILE_TYPES = local_constants.select { |const| const.to_s.starts_with? 'VFILE_TYPE_' }
 
-		mount_uploader :file, FileUploader
+    mount_uploader :file, FileUploader
 
-		def self.files_by_type(type)
-			where parent_type: type
-		end
+    def self.files_by_type(type)
+      where parent_type: type
+    end
 
-		def file_url
-			file.try(:url)
-		end		
-	end
+    def file_url
+      file.try(:url)
+    end
+  end
 end
--- app/models/rhinoart/page_field.rb
+++ app/models/rhinoart/page_field.rb
@@ -11,31 +11,31 @@
 #
 
 module Rhinoart
-	class PageField < Rhinoart::Base
-		self.inheritance_column = "non_sti"
-		after_save :update_page_date
-		after_destroy :update_page_date
+  class PageField < Rhinoart::Base
+    self.inheritance_column = 'non_sti'
+    after_save :update_page_date
+    after_destroy :update_page_date
 
-		belongs_to :page, :inverse_of => :page_field	
-		accepts_nested_attributes_for :page
+    belongs_to :page, inverse_of: :page_field
+    accepts_nested_attributes_for :page
 
-		has_one :attachment, as: :attachable, class_name: "Rhinoart::Files", :autosave => true, :dependent => :destroy
-		accepts_nested_attributes_for :attachment, allow_destroy: true #, reject_if: :all_blank
+    has_one :attachment, as: :attachable, class_name: 'Rhinoart::Files', autosave: true, dependent: :destroy
+    accepts_nested_attributes_for :attachment, allow_destroy: true # , reject_if: :all_blank
 
-		default_scope { order 'position' }
-		acts_as_list scope: :page_id
+    default_scope { order 'position' }
+    acts_as_list scope: :page_id
 
-		FIELD_TYPES = { text: 'Text', textarea: 'Textarea', file: 'File', boolean: 'Bollean', title: 'Title', meta: 'Meta descr and key' }
+    FIELD_TYPES = { text: 'Text', textarea: 'Textarea', file: 'File', boolean: 'Bollean', title: 'Title', meta: 'Meta descr and key' }.freeze
 
-		validates :name, :ftype, presence: true
-		validates_uniqueness_of :name, :scope => :page_id
+    validates :name, :ftype, presence: true
+    validates_uniqueness_of :name, scope: :page_id
 
-		validates :ftype, inclusion: { in: FIELD_TYPES.keys.map(&:to_s) }
-	
-		has_paper_trail
-		
-		def select_list
-			FIELD_TYPES.map { |ft| [ft[1], ft[0]] }
-		end
-	end
+    validates :ftype, inclusion: { in: FIELD_TYPES.keys.map(&:to_s) }
+
+    has_paper_trail
+
+    def select_list
+      FIELD_TYPES.map { |ft| [ft[1], ft[0]] }
+    end
+  end
 end
